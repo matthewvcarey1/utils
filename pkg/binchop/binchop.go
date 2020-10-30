acea8aa26296fd52623f7a0b3387358d9d1bf423
Matthew Carey <Matthew.Carey@gbgplc.com>
2020-10-30 20:58:41 +0000

    Added some documentation

--- pkg/binchop/binchop.go
+++ pkg/binchop/binchop.go
@@ -4,10 +4,12 @@ import (
 	"strings"
 )
 
+// An interface that describes the function neeed by BinChop
 type SliceElementComparitor interface {
 	Cmp(model interface{}, index int) int
 }
 
+// A type agnostic BinChop implementation
 func BinChop(value interface{}, start int, end int, sec SliceElementComparitor) (bool, int) {
 	for {
 		size := end - start
@@ -33,29 +35,33 @@ func BinChop(value interface{}, start int, end int, sec SliceElementComparitor) 
 	}
 }
 
-
+// struct used for searching sorted slices of strings
 type stringSliceComp struct {
 	slice []string
 }
 
+// Cmp compare the model string with one at an index of the slice
 func (sc stringSliceComp) Cmp(model interface{}, index int) int {
 	return strings.Compare(model.(string), sc.slice[index])
 }
 
+// struct used for seaching sorted slices of ints
 type intSliceComp struct {
 	slice []int
 }
 
+// Cmp compare the model int with one at an index of the slice
 func (ic intSliceComp) Cmp(model interface{}, index int) int {
 	return model.(int) - ic.slice[index]
 }
 
-
+// BinChopInts convenience function to search ordered slices of ints
 func BinChopInts(value int, slice []int) (bool, int){
 	isc := intSliceComp{slice}
 	return BinChop(value, 0, len(slice), isc)
 }
 
+// BinChopInts convenience function to search ordered slices of strings
 func BinChopStrings(value string, slice []string) (bool, int){
 	ssc := stringSliceComp{slice}
 	return BinChop(value, 0, len(slice), ssc)
--- pkg/qsort/quicksort.go
+++ pkg/qsort/quicksort.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 )
 
+// An interface of the object passed to Quicksort
 type Sorter interface {
 	Swap(a int, b int)
 	Child(start int, end int) Sorter
@@ -12,6 +13,7 @@ type Sorter interface {
 	Len() int
 }
 
+// A type agnostic implentation of Quicksort
 func Quicksort(s Sorter) {
 	length := s.Len()
 	if length < 2 {
@@ -35,49 +37,66 @@ func Quicksort(s Sorter) {
 
 }
 
-// Convenience functions
+// Convenience functions and types
 
+// The structure need for string sorting
 type stringSorter struct {
 	slice []string
 }
 
+// Swap swaps two strings in a slice
 func (ss *stringSorter) Swap(a int, b int) {
 	ss.slice[a], ss.slice[b] = ss.slice[b], ss.slice[a]
 }
+
+// Compare two strings in a slice
 func (ss *stringSorter) Compare(a int, b int) int {
 	return strings.Compare(ss.slice[a], ss.slice[b])
 }
+
+// Child returns a stringSorter with a sub slice
 func (ss *stringSorter) Child(start int, end int) Sorter {
 	return &stringSorter{ss.slice[start:end]}
 }
+
+// Len returns the stringSorter's slice length
 func (ss *stringSorter) Len() int {
 	return len(ss.slice)
 }
 
-
+// QuickSortStrings a convenience fuction for sorting a slice of strings
 func QuickSortStrings(slice []string)  []string{
 	 ss := stringSorter{slice}
 	 Quicksort(&ss)
 	 return slice
 }
 
+// A structure for int sorting
 type intSorter struct {
 	slice []int
 }
 
+// Swap swaps two ints in a slice
 func (is *intSorter) Swap(a int, b int) {
 	is.slice[a], is.slice[b] = is.slice[b], is.slice[a]
 }
+
+// Compare two ints in a slice
 func (is *intSorter) Compare(a int, b int) int {
 	return is.slice[a] - is.slice[b]
 }
+
+// Child returns an intSorter with a sub slice
 func (is *intSorter) Child(start int, end int) Sorter {
 	return &intSorter{is.slice[start:end]}
 }
+
+// Len returns the intSorter's slice length
 func (is *intSorter) Len() int {
 	return len(is.slice)
 }
 
+// QuickSortStrings a convenience fuction for sorting a slice of ints
 func QuickSortInts(slice []int)  []int{
 	is := intSorter{slice}
 	Quicksort(&is)
